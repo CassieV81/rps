00044931f916aaf2a5f0759b1fb237a8301253b8
CassieV <obicassievirtue@gmail.com>
2022-05-09 17:41:37 +0200

    create function

---
+++ index.js
@@ -0,0 +1,7 @@
+const game = ['rock', 'paper', 'scissors'];
+const computerPlay = game[Math.floor(Math.random() * game.length)];
+console.log(computerPlay);
+
+function gamePlay(playerSelection, computerSelection) {
+
+}
