434862c68d5fea88e68046afdf17c93ec11742e7
CassieV <obicassievirtue@gmail.com>
2022-05-09 20:39:37 +0200

    Enter game rules

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const game = ['rock', 'paper', 'scissors'];
-const computerPlay = game[Math.floor(Math.random() * game.length)];
-console.log(computerPlay);
-
-function gamePlay(playerSelection, computerSelection) {
-
+function computerPlay() {
+    return game[Math.floor(Math.random() * game.length)];
 }
+
+
+
+
+
+function gamePlay(playerSelection, computerSelection) { 
+    
+
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You loose! Paper beats Rock';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win! Paper beats Rock';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You loose! Rock crushes Scissors';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win! Rock crushes Scissors'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You loose! Scissors cuts Paper'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You win! Scissors cuts Paper'
+    } 
+    else {
+        return `${playerSelection} beats ${computerSelection}`
+    }
+}
+
+
+const playerSelection = 'paper';
+const computerSelection = computerPlay();
+console.log(gamePlay(playerSelection, computerSelection));
+
